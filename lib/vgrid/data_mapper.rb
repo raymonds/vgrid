6d4e199341e0b42ec9141ee645a36bd5259cc387
vigram <vigram.k@gmail.com>
2014-06-29 22:28:22 +0530

    Basic Grid design in progress

--- lib/vgrid/data_mapper.rb
+++ lib/vgrid/data_mapper.rb
@@ -1,1 +1,14 @@
 # Define mapping b/w grid and data source
+module Vgrid
+  class DataMapper
+
+    def self.map_data(options = {})
+      options = {model: employee, columns: ['id', 'name'], rows: 10}
+      # Employee.search.select(options[:columns]).limit(rows).each { |row|
+      #  
+      #
+      # }
+    end
+
+  end
+end
--- lib/vgrid/vgrid.rb
+++ lib/vgrid/vgrid.rb
@@ -1,6 +1,13 @@
 module Vgrid
   class Grid
 
+    def initialize(model = nil, columns = [], options = {})
+      # columns = [{name: empName, type: 'text', sortable: true, searchable: true, editable: true}]
+      # options = {model: employee, actions: {add: true, delete: true, save: true, refresh: true, export: true},
+      #            cache: true, expiry_interval: 10, row_limit = 10}
+
+    end
+
   end
 
 end
